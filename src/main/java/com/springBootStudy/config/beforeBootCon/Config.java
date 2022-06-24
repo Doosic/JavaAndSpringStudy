a7e73a48ff6d2e131dc6ebe7ebc302c58d6e3e4a
DoosicEE <strou73@naver.com>
2022-06-25 01:50:24 +0900

    #12-Component 와 Configuration 의 차이
    솔직히 잘 모르겠다. 새벽 2시가 다되어가서 그런가 집중이 안된다. 자고 일어나서 다시 정리하는 시간을 가져야 겠다.

--- src/main/java/com/springBootStudy/config/beforeBootCon/Config.java
+++ src/main/java/com/springBootStudy/config/beforeBootCon/Config.java
@@ -1,5 +1,8 @@
 //package com.springBootStudy.config;
 //
+//import com.springBootStudy.logic.BubbleSort;
+//import com.springBootStudy.logic.Sort;
+//import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.ComponentScan;import org.springframework.context.annotation.ComponentScans;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,4 +11,10 @@
 //@ComponentScan("com.springBootStudy")
 //@Configuration
 //public class Config {
+//
+//    @Bean
+//    public Sort<String> bubbleSort() {
+//        return new BubbleSort<>();
+//    }
+//
 //}
