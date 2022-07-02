742f6762971e5e440472b5713ade8939f25fbc73
DoosicEE <strou73@naver.com>
2022-07-02 23:54:26 +0900

    #16 vault/실무를 고급지게 만들어주는 기능 마무리
    vault 사용법에 대해서 배워보았다.
    토이프로젝트에 한번 사용해볼 예정이다.

--- src/main/java/com/springBootStudy/service/StudentService.java
+++ src/main/java/com/springBootStudy/service/StudentService.java
@@ -3,35 +3,28 @@ package com.springBootStudy.service;
 import com.springBootStudy.StudentEntity;
 import com.springBootStudy.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
-
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class StudentService {
 
     private final StudentRepository studentRepository;
 
-    @Cacheable("StudentEntity")
     public void printStudent(String name){
         StudentEntity studentEntity = studentRepository.getStudent(name);
-        System.out.println("찾는 학생 "+ studentEntity);
+        log.info("찾는 학생~ "+ studentEntity);
     }
 
     @PostConstruct
     public void init() {
         studentRepository.enroll("jack", 15, StudentEntity.Grade.E);
-        studentRepository.enroll("jack", 15, StudentEntity.Grade.E);
-        studentRepository.enroll("jack", 15, StudentEntity.Grade.E);
         studentRepository.enroll("fred", 18, StudentEntity.Grade.A);
-        studentRepository.enroll("fred", 18, StudentEntity.Grade.A);
-        studentRepository.enroll("fred", 18, StudentEntity.Grade.A);
-        studentRepository.enroll("fred", 18, StudentEntity.Grade.A);
-        studentRepository.enroll("cache", 17, StudentEntity.Grade.C);
         studentRepository.enroll("cache", 17, StudentEntity.Grade.C);
     }
 }
---
+++ src/test/java/com/springBootStudy/SpringBootStudyApplicationTest.java
@@ -0,0 +1,16 @@
+package com.springBootStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SpringBootStudyApplicationTest {
+
+    @Test
+    void contextLoads(){
+
+    }
+
+}
