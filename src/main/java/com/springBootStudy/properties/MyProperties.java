503d10fd18012631eefe186c35bb71bd15cdac6a
DoosicEE <strou73@naver.com>
2022-07-09 01:34:39 +0900

    #21 아는 사람만 아는Boot 기능 마무리
    redis, vualt, configServer 까지 많은걸 배웠다. 정리하고 토이프로젝트때 다 적용해보아야겠다.
    복습하고 정리하는것도 힘들겠다 ... ㅎㅎ

--- src/main/java/com/springBootStudy/properties/MyProperties.java
+++ src/main/java/com/springBootStudy/properties/MyProperties.java
@@ -4,9 +4,11 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @Getter
 @ConstructorBinding
+@RefreshScope // 리프레쉬를 하게되면 다시 빈을 읽어서 등록하는 기능
 //@ConfigurationProperties("api.custom.properties")
 @RequiredArgsConstructor
 @ConfigurationProperties("my")
--- src/main/java/com/springBootStudy/service/MyService.java
+++ src/main/java/com/springBootStudy/service/MyService.java
@@ -1,8 +1,10 @@
 package com.springBootStudy.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
+@RefreshScope // 리프레쉬를 하게되면 다시 빈을 읽어서 등록하는 기능
 @Service
 public class MyService {
 
